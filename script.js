5d90ff0c4d8992cabafb48c3b56513cccfeb82f0
Joe Small <small.joseph@outlook.com>
2021-03-03 19:46:13 -0500

    changes to html

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-$( function() {
-    $( "#datepicker" ).datepicker();
-  } );
-// button
+
 	// get a refernce to all of the save buttons $('.saveBtn')
+	$(".saveBtn");
+
 	// 01-Ins_jQuery-Elements and 02-Stu_jQuery-Elements
-​
+	$(".saveBtn").on("click", function () {
+
+	});
+
 	// add a click handler to each save Button using .on() method
+
 	// 03-Ins_Click-Events and 04-Stu_Click-Events - https://api.jquery.com/on/
 ​
 	// traverse the DOM to get values that we need to store in localStorage .parent() .sibling()
 	// 07-Ins_Dom-Traversal and 08-Stu_Dom-Traversal - https://api.jquery.com/category/traversing/
-​
 	// setItems in localStorage
 	// unit 04
 ​
@@ -19,8 +21,35 @@ $( function() {
 	// https://momentjs.com/docs/#/get-set/hour/
 ​
 	//  get a refernce to all timeblocks
-​
+​	$(".timeblock")
+for (let index = 0; index < array.length; index++) {
+	const element = array[index];
+}
+
+// set items to local storage
+localStorage.setItem("#nine", ".description");
+localStorage.setItem("#ten", ".description");
+localStorage.setItem("#eleven", ".description");
+localStorage.setItem("#twelve", ".description");
+localStorage.setItem("#one", ".description");
+localStorage.setItem("#two", ".description");
+localStorage.setItem("#three", ".description");
+localStorage.setItem("#four", ".description");
+localStorage.setItem("#five", ".description");
+
+
+// get items from local storage
+$("#nine").val(localStorage.getItem("#nine"));
+$("#ten").val(localStorage.getItem("#ten"));
+$("#eleven").val(localStorage.getItem("#eleven"));
+$("twelve").val(localStorage.getItem("#twelve"));
+$("#one").val(localStorage.getItem("#one"));
+$("#two").val(localStorage.getItem("#two"));
+$("#three").val(localStorage.getItem("#three"));
+$("#four").val(localStorage.getItem("#four"));
+$("#five").val(localStorage.getItem("#five"));
 	// loop through the timeblocks
+	// **maybe turn them into an array first?
 	// unit 03
 ​
 		// get the data-hour value timeblocks[i].data('hour')
@@ -33,7 +62,7 @@ $( function() {
 ​
 // get the data from localstorage and populate the timeblocks
 	// getItem
-​
+
 	// reference to the textarea (id)
 ​
 	// .val()
